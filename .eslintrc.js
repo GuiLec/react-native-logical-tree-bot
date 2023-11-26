44ecd7182e205fb1a959d6c03437bbdb199059c3
GuiLec <guillaumele@bam.tech>
2023-11-26 13:45:50 +0100

    setup linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,28 @@
-module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+const { defineConfig } = require("eslint-define-config");
+
+module.exports = defineConfig({
+  extends: [
+    // see source: https://github.com/bamlab/react-native-project-config/blob/main/packages/eslint-plugin/lib/configs/recommended.js
+    "plugin:@bam.tech/recommended",
+    "plugin:import/typescript",
+  ],
+  plugins: ["import"],
+  overrides: [
+    {
+      // config files that use CommonJS
+      files: [".eslintrc.js", "prettier.config.js", "app.config.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+  rules: {
+    "react-native/no-raw-text": "off",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
+});
